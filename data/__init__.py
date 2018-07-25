a0050d6158e96b7bcbab1ba5126684d196d9adf6
tosin <tosin.sonuyi@gmail.com>
2018-07-25 02:30:24 -0400

    add importable df for per stats

--- data/__init__.py
+++ data/__init__.py
@@ -2,3 +2,4 @@ import os
 import pandas as pd
 
 SALARY_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__),'salary_contract_player.csv'))
+PER_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__),'PER_player_career_regszn.csv'))
