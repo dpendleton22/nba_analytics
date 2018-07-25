fb7e15a1a89f45a09840cf53ed24313bac8a435b
tosin <tosin.sonuyi@gmail.com>
2018-07-25 01:45:53 -0400

    add PER player stats data

---
+++ contract_comp_viz.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+from data import SALARY_DATA
+
+
+"""
+Resources:
+https://www.basketball-reference.com/contracts/players.html
+https://www.reddit.com/r/nba/comments/6dvmr1/best_advanced_stat_to_measure_how_good_a_player_is/
+https://fansided.com/2017/01/31/nylon-calculus-reinventing-per/
+
+Looks like best all in one stats are
+PER (Player Efficiency Rating)
+WS (Win Shares)
+RPM (Real Plus Minus)
+BPM (?)
+"""
+
