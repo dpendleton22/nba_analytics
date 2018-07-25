111fe5b12e4690d6191e2ae249ae12dd979e48eb
tosin <tosin.sonuyi@gmail.com>
2018-07-25 02:31:12 -0400

    fist draft at joining data and make colorized scatter plot

--- contract_comp_viz.py
+++ contract_comp_viz.py
@@ -1,7 +1,9 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from data import SALARY_DATA, PER_DATA
 
-from data import SALARY_DATA
 
 
 """
@@ -16,4 +18,40 @@ WS (Win Shares)
 RPM (Real Plus Minus)
 BPM (?)
 """
+
+# get rid of the stuff to the right of \ i.e. LeBron James\jamesle01
+standardize = lambda f:f.rsplit('\\')[0].lower()
+# make money a float not a string
+def monify(m):
+	try:
+		return float(m.split('$')[1])
+	except:
+		return m
+
+SALARY_DATA['Player'] = SALARY_DATA['Player'].map(standardize)
+SALARY_DATA['Guaranteed'] = SALARY_DATA['Guaranteed'].map(monify)
+PER_DATA['Player'] = PER_DATA['Player'].map(standardize)
+
+
+
+salary_cols = ['Player','Tm','Signed Using','Guaranteed']
+per_cols = ['Rank','Player','PER']
+
+df = SALARY_DATA[salary_cols].merge(PER_DATA[per_cols], on='Player')
+
+"""
+Visualization
+"""
+
+sns.set(style="whitegrid")
+f, ax = plt.subplots(figsize=(6.5, 6.5))
+
+contract_ranking = ["Early Bird", "Cap space", "1st Round Pick", "Non-Bird rights", "MLE", "Bi-annual Exception", "Minimum Salary"]
+sns.scatterplot(x="PER", y="Guaranteed",
+                hue="Signed Using",
+                # hue_order=contract_ranking,
+                sizes=(1, 8), linewidth=0,
+                data=df, ax=ax)
+
+plt.show()
 
