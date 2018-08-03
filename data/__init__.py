1b347999af042e73f174b45d033ada369fbd4c79
tosin <tosin.sonuyi@gmail.com>
2018-08-02 21:50:24 -0400

    add team data w/ 3 letter codes

--- data/__init__.py
+++ data/__init__.py
@@ -4,3 +4,4 @@ import pandas as pd
 SALARY_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__),'salary_contract_player.csv'))
 PER_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__),'PER_player_career_regszn.csv'))
 BPM_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__),'BPM_player_career_regszn.csv'))
+TM_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__),'team_data.csv'))
